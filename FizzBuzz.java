0f90b2f7302ded19560a558e0723e4c5dbf376b0
Raekian <Stevenzerwas@gmail.com>
2021-08-03 10:02:02 -0500

    init

---
+++ FizzBuzz.java
@@ -0,0 +1,12 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if (number % 15 == 0) {
+            return "fizzbuzz";
+        } else if  (number % 5 == 0) {
+            return "buzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
+        }
+        return String.valueOf(number);
+    }
+}
